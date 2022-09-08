0714a64e8b2179d9d5114aeff11e1e5648f3c8ca
NBNgoc2102 <nbngoc2102@gmail.com>
2022-09-08 14:58:49 +0700

    update

--- asses/js/main.js
+++ asses/js/main.js
@@ -22,7 +22,7 @@ function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
     document.getElementById("navbar").style.top = "0";
   } else {
-    document.getElementById("navbar").style.top = "-90px";
+    document.getElementById("navbar").style.top = "-120px";
   }
 }
 
